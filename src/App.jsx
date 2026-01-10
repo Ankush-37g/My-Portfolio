6274d410eda2a5ad2b267fd18e54b9ae98bb4247
Ankush Sharma <assharmaankush56@gmail.com>
2026-01-10 13:36:27 +0530

    intial-setup

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import react from 'react'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+import {useState, useEffect} from 'react'
+
+const App = () => {
+   
+   return (
+      <div>
+         <h1 class="text-3xl font-bold underline">
+    Hello world!
+        </h1>
+
+      </div>
+   )
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+ import { div } from "framer-motion/client";
+import React from "react";
+
+ const Navbar = () => {
+   return (
+     <div>
+        
+     </div>
+   )
+}
+
+export default Navbar;
