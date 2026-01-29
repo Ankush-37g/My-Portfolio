eb69e2d6a9da33b12abf35e34d30b8fd9c41a493
Ankush Sharma <assharmaankush56@gmail.com>
2026-01-29 16:33:17 +0530

    Added assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,29 @@
-import AOS from 'aos'
-import 'aos/dist/aos.css'
-import {useState, useEffect} from 'react'
-import Navbar from './components/Navbar.jsx'
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useState, useEffect } from "react";
+import Navbar from "./components/Navbar.jsx";
 
 const App = () => {
-   const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useState(true);
 
-   useEffect(()=>{
-     AOS.init({
-        duration: 1000,
-        once: false,
-        offset: 100
-     });
-     document.documentElement.classList.add('dark')
-   },[]);
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      once: false,
+      offset: 100,
+    });
+    document.documentElement.classList.add("dark");
+  }, []);
 
-   const toggleDarkMode = () => {
-      setDarkMode(!darkMode);
-      document.documentElement.classList.toggle('dark');
-   }
-   return (
-      <div>
-       
-         <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode}/>
-
-      </div>
-   )
-}
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+    document.documentElement.classList.toggle("dark");
+  };
+  return (
+    <div>
+      <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+    </div>
+  );
+};
 
 export default App;
