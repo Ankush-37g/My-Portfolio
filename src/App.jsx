6d115de3ee5c82c014664545d0ccd96aafb21142
Ankush Sharma <assharmaankush56@gmail.com>
2026-01-30 23:06:17 +0530

    added 3d galaxy bacground to all components

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 import { useState, useEffect } from "react";
 import Navbar from "./components/Navbar.jsx";
+import Hero from "./components/Hero.jsx";
+import Galaxy from "./3dcomponents/galaxy.jsx";
 
 const App = () => {
   const [darkMode, setDarkMode] = useState(true);
@@ -15,14 +17,51 @@ const App = () => {
     document.documentElement.classList.add("dark");
   }, []);
 
+  useEffect(()=> {
+    AOS.refresh();
+  }, [darkMode]);
+
   const toggleDarkMode = () => {
     setDarkMode(!darkMode);
     document.documentElement.classList.toggle("dark");
   };
   return (
-    <div>
-      <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+    <div className="relative min-h-screen overflow-hidden">
+
+        {/* Galaxy Background (Only Dark Mode) */}
+        {darkMode && (
+          <>
+            <div className="fixed inset-0 z-0">
+              <Galaxy
+                mouseInteraction
+                mouseRepulsion
+                density={1}
+                glowIntensity={0.3}
+                hueShift={140}
+                starSpeed={0.4}
+              />
+            </div>
+
+            {/* Overlay */}
+            <div className="fixed inset-0 bg-black/40 z-[1]"></div>
+          </>
+        )}
+
+        {/* Main Content */}
+        <div className="relative z-10">
+
+          <Navbar darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
+
+          <Hero darkMode={darkMode} />
+          {/* <About darkMode={darkMode} />
+          <Skills darkMode={darkMode} />
+          <Projects darkMode={darkMode} />
+          <Contact darkMode={darkMode} /> */}
+
+        </div>
+
     </div>
+   
   );
 };
 
