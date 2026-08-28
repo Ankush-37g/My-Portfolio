0bbabbbd7588abf7509276eacb2172dea14ac522
Ankush Sharma <assharmaankush56@gmail.com>
2026-08-28 12:01:18 +0530

    Fixed email sending issue

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -56,15 +56,16 @@ const Contact = ({ darkMode }) => {
             try {
                 // Send email using EmailJS
                 const response = await emailjs.send(
-                    import.meta.env.VITE_EMAILJS_SERVICE_ID ,
-                    import.meta.env.VITE_EMAILJS_TEMPLATE_ID ,
+                    import.meta.env.VITE_EMAILJS_SERVICE_ID,
+                    import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
                     {
                         from_name: `${formData.firstname} ${formData.lastname}`,
                         from_email: formData.email,
                         phone: formData.phone,
                         message: formData.message,
                         to_email: import.meta.env.VITE_EMAILJS_TO_EMAIL
-                    }
+                    },
+                    import.meta.env.VITE_EMAILJS_PUBLIC_KEY
                 );
 
                 if (response.status === 200) {
